bf1cba3a10fa87b97e63c278a8317689cd068559
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:30:52 +0530

    added: Creating the desired BST and checking its size

--- Program.cs
+++ Program.cs
@@ -8,9 +8,25 @@
             bst.Insert(56);
             bst.Insert(30);
             bst.Insert(70);
+            bst.Insert(22);
+            bst.Insert(40);
+            bst.Insert(11);
+            bst.Insert(16);
+            bst.Insert(3);
+            bst.Insert(60);
+            bst.Insert(95);
+            bst.Insert(65);
+            bst.Insert(63);
+            bst.Insert(67);
 
             Console.WriteLine("Inorder Traversal of BST:");
             bst.InorderTraversal(bst.root);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Size of BST: " + bst.Size(bst.root));
+
+            
+           
         }
     }
 }
