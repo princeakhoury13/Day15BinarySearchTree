39f391343883febb273f681aae1dc23c072d9978
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:17:29 +0530

    added: New binary search tree logic

---
+++ BinarySearchTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+
+                while (true)
+                {
+                    parent = current;
+
+                    if (data < current.data)
+                    {
+                        current = current.left;
+
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void InorderTraversal(Node node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.left);
+                Console.Write(node.data + " ");
+                InorderTraversal(node.right);
+            }
+        }
+    }
+}
