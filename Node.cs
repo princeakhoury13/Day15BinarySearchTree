ad10dde9ef7624fe969226b837fc97cf46d7ccb3
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:17:05 +0530

    added: created new node

---
+++ Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15BinarySearchTree
+{
+    public class Node
+    {
+        public int data;
+        public Node left;
+        public Node right;
+
+        public Node(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+}
