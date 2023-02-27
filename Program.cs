643a58eababec16d333edd439c14e3a571a9f68b
princeakhoury13 <princepraveen130509@gmail.com>
2023-02-27 11:13:19 +0530

    added: Master Branch greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day15BinarySearchTree
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Master Branch for Day 15 assignment solutions");
+        }
+    }
+}
